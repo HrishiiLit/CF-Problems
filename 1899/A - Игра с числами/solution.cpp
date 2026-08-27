4f1b1a45d2a4270455ac019012654d52a51f5a4d
Hrishikesh Patil <hrishikeshpatil5002@gmail.com>
2026-08-27 21:53:43 +0530

    Add Игра с числами [A] from Codeforces

---
+++ 1899/A - Игра с числами/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int tc;
+    cin>>tc;
+    
+    while(tc--) {
+        int num;
+        cin>>num;
+        if(num%3 != 0){
+            cout<<"First
+";
+        }
+        else{
+            cout<<"Second
+";
+        }
+    }
+    
+    return 0;
+}
